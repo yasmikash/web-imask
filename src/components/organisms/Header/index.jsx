d2507409744906efce1426523779388461a8ceec
yasmikash <yashtricksz@gmail.com>
2022-07-14 07:49:19 +0530

    remove additional button

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -11,9 +11,6 @@ const Header = () => {
       <div className="w-[95px] h-[46px] bg-[url('/src/assets/images/imask.png')] bg-contain bg-no-repeat"></div>
       <div className="flex justify-center items-center gap-2">
         <CircleButton active icon={HomeIcon} />
-        <div className="absolute rounded-xl px-2 text-white text-[10px] p-1 bg-red-500 opacity-20">
-          COVID-19 STATUS: 51%
-        </div>
         <div className="rounded-xl px-2 text-white text-[10px] p-1 bg-red-500">
           COVID-19 STATUS: 51%
         </div>
