0e38cfebdebdb94da56d9988846ff627bf2ffd4d
yasmikash <yashtricksz@gmail.com>
2022-09-03 17:13:24 +0530

    ehgearhgeargh

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const path = require("path");
-const https = require("https");
-const fs = require("fs");
+// const https = require("https");
+// const fs = require("fs");
 const express = require("express");
 const app = express();
 
-const privateKey = fs.readFileSync("private.key");
-const certificate = fs.readFileSync("certificate.crt");
-const ca = fs.readFileSync("ca_bundle.crt");
+// const privateKey = fs.readFileSync("private.key");
+// const certificate = fs.readFileSync("certificate.crt");
+// const ca = fs.readFileSync("ca_bundle.crt");
 
 const APP_PORT = 80;
 
@@ -21,10 +21,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// app.listen(APP_PORT, () => {
-//   console.log(`APP STARTS ON PORT ${APP_PORT}`);
-// });
-
-https
-  .createServer({ key: privateKey, cert: certificate, ca: ca }, app)
-  .listen(APP_PORT, () => console.log("App starts"));
+app.listen(APP_PORT, () => {
+  console.log(`APP STARTS ON PORT ${APP_PORT}`);
+});
