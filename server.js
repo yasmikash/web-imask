c8e3954bb2ef607a15e1a7d22b9e524b943ad59b
yasmikash <yashtricksz@gmail.com>
2022-09-03 16:52:21 +0530

    add ssl

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const path = require("path");
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const app = express();
+
+const privateKey = fs.readFileSync("private.key");
+const certificate = fs.readFileSync("certificate.crt");
 
 const APP_PORT = 80;
 
@@ -15,6 +20,10 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(APP_PORT, () => {
-  console.log(`APP STARTS ON PORT ${APP_PORT}`);
-});
+// app.listen(APP_PORT, () => {
+//   console.log(`APP STARTS ON PORT ${APP_PORT}`);
+// });
+
+https
+  .createServer({ key: privateKey, cert: certificate }, app)
+  .listen(APP_PORT);
