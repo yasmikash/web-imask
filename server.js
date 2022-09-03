bf2aecbad6b3c4db750954c65d922362e94da6cf
yasmikash <yashtricksz@gmail.com>
2022-09-03 14:26:10 +0530

    server add

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const APP_PORT = 80;
 
 app.use(express.static("dist"));
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
